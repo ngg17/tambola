908f15c1863b107100fa4c501190c693ebf7503a
Noyonika Gupta <71960903+ngg17@users.noreply.github.com>
2022-09-18 10:08:25 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -4,26 +4,33 @@ import time, random
 
 SERVER = None
 IP_ADDRESS = '127.0.0.1'
-PORT = 8000
+PORT = 8080
 
 CLIENTS = {}
 flashNumberList =[ i  for i in range(1, 91)]
 
+gameOver = False
 playersJoined = False
 
 
-
+#
 def handleClient():
     global CLIENTS
     global flashNumberList
+    global gameOver
     global playersJoined
 
     while True:
+        if(gameOver):
+            break
+
         try:
-            if(len(list(CLIENTS.keys())) >=2):
+            # Atleast two player required to play this game
+            if(len(list(CLIENTS.keys())) >=2 and not gameOver):
                 if(not playersJoined):
                     playersJoined = True
                     time.sleep(1)
+
 
 
                 if(len(flashNumberList) > 0):
@@ -37,13 +44,33 @@ def handleClient():
 
                         flashNumberList.remove(int(randomNumber))
                     except:
+                        # Removing Player cleint when they close / terminate the session
                         del CLIENTS[currentName]
 
+                    # After Every 3 Seconds we are sending one number to each CLIENT
                     time.sleep(3)
+                else:
+                    gameOver = True
+        except:
+            gameOver = True
 
+
+
+def recvMessage(player_socket):
+    global CLIENTS
+    global gameOver
+
+    while True:
+        try:
+            message = player_socket.recv(2048).decode()
+            if(message):
+                for cName in CLIENTS:
+                    cSocket = CLIENTS[cName]["player_socket"]
+                    if('wins the game.' in message):
+                        gameOver = True
+                    cSocket.send(message.encode())
         except:
             pass
-
 
 
 
@@ -62,10 +89,13 @@ def acceptConnections():
 
         print(f"Connection established with {player_name} : {addr}")
 
+        thread1 = Thread(target = recvMessage, args=(player_socket,))
+        thread1.start()
+
+
 
 def setup():
     print("\n\t\t\t\t\t*** Welcome To Tambola Game ***\n")
-
 
     global SERVER
     global PORT
@@ -88,3 +118,7 @@ def setup():
 
 setup()
 
+#CHANGES  MADE
+# recvMessage from CLIENT
+# add game over Functionality in handleClient method
+# when one of the player wins the game change game over to True for all the Players
